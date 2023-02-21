d682861ac0f542c7e9009532b9847842c7a35293
Artemkaaas <artem.ivanov@dsr-corporation.com>
2023-02-21 09:18:44 +0300

    Updated parside to use the latest cesride

--- src/message/groups/attached_material_quadlets.rs
+++ src/message/groups/attached_material_quadlets.rs
@@ -1,24 +1,24 @@
-use crate::error::ParsideResult;
+use crate::error::{ParsideError, ParsideResult};
 use crate::message::cold_code::ColdCode;
+use crate::message::{Group, GroupItem};
 use cesride::counter::Codex;
-use cesride::{Counter, Matter};
-use crate::message::Group;
+use cesride::{Counter, Siger, Indexer};
 
 
 // FIXME: Implement proper definition
 #[derive(Debug, Clone, Default)]
 pub struct AttachedMaterialQuadlets {
-    pub value: Vec<Matter>,
+    pub value: Vec<AttachedMaterialQuadlet>,
 }
 
-impl Group<Matter> for AttachedMaterialQuadlets {
-    const CODE: Codex = Codex::AttachedMaterialQuadlets;
+impl Group<AttachedMaterialQuadlet> for AttachedMaterialQuadlets {
+    const CODE: &'static str = Codex::AttachedMaterialQuadlets;
 
-    fn new(value: Vec<Matter>) -> Self {
+    fn new(value: Vec<AttachedMaterialQuadlet>) -> Self {
         Self { value }
     }
 
-    fn value(&self) -> &Vec<Matter> {
+    fn value(&self) -> &Vec<AttachedMaterialQuadlet> {
         &self.value
     }
 }
@@ -30,5 +30,31 @@ impl AttachedMaterialQuadlets {
         _cold_code: &ColdCode,
     ) -> ParsideResult<(&'a [u8], AttachedMaterialQuadlets)> {
         unimplemented!();
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct AttachedMaterialQuadlet {
+    pub siger: Siger
+}
+
+impl AttachedMaterialQuadlet {
+    pub fn new(siger: Siger) -> Self {
+        Self { siger }
+    }
+}
+
+
+impl GroupItem for AttachedMaterialQuadlet {
+    fn qb64(&self) -> ParsideResult<String> {
+        self.siger.qb64().map_err(ParsideError::from)
+    }
+
+    fn qb64b(&self) -> ParsideResult<Vec<u8>> {
+        self.siger.qb64b().map_err(ParsideError::from)
+    }
+
+    fn qb2(&self) -> ParsideResult<Vec<u8>> {
+        self.siger.qb2().map_err(ParsideError::from)
     }
 }
